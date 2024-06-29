26283a991ed9620ea7fcba2f90a8169c597155d1
avsk80 <avskkrish80@gmail.com>
2024-06-29 00:44:53 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Venkata Sai Krishna Abbaraju',
+    author='The aim of this project is to predict the trip duration for the taxis in New York city',
+    license='',
+)
